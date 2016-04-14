1a54aad707f1e008df994a161e12ddb4ed114bbd
maksym.koretskyi <m.koretskyi@gmail.com>
2016-04-14 20:07:48 +0300

    Fixed #5806: Modal dialog closes when mouse button is pressed over the dialog window but released outside the window

--- src/modal/modal.js
+++ src/modal/modal.js
@@ -157,8 +157,18 @@ angular.module('ui.bootstrap.modal', ['ui.bootstrap.stackedMap', 'ui.bootstrap.p
           }
         };
 
-        // moved from template to fix issue #2280
-        element.on('click', scope.close);
+        var pointerDownElement;
+        element.on('mousedown', function (event) {
+          pointerDownElement = event.target;
+        });
+
+        element.on('mouseup', function (event) {
+          var element = event.target;
+
+          if (element === pointerDownElement) {
+            scope.close(event);
+          }
+        });
 
         // This property is only added to the scope for the purpose of detecting when this directive is rendered.
         // We can detect that by using this property in the template associated with this directive and then use
